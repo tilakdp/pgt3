35995649cc9cf554e1225a090857ea2997690f10
tilak dp <tilak_dp@yahoo.com>
2022-04-15 21:46:29 +0530

    update addguest.php

--- addguest.php
+++ addguest.php
@@ -2,16 +2,16 @@
     include('session.php');
 ?>
 <html>
-    <head>
-        <title>add occupant</title>
-    </head>
-    <body>
-        <div>
-            <form>
-                <input type="text" name="Full Name">
-                <input type="text" name="Nick">
-                <input type="date" name="doj">
-            </form>
-        </div>
-    </body>
+<head>
+    <title>add occupant</title>
+</head>
+<body>
+    <div>
+        <form>
+            <input type="text" name="Full Name">
+            <input type="text" name="Nick">
+            <input type="date" name="doj">
+        </form>
+    </div>
+</body>
 </html>
