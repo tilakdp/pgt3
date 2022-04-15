6b0aba415f2014e3de8cbdf6a7649346c8793cf1
tilak dp <tilak_dp@yahoo.com>
2022-04-15 21:36:23 +0530

    update addguest.php

---
+++ addguest.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>add occupant</title>
+    </head>
+    <body>
+        <div>
+            <form>
+                <input type="text" name="Full Name">
+                <input type="text" name="Nick">
+                <input type="date" name="doj">
+            </form>
+        </div>
+    </body>
+</html>
