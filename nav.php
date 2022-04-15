9f1268ee9816141c4f3ee0e73ef48e42f959b061
tilak dp <tilak_dp@yahoo.com>
2022-04-15 10:44:44 +0530

    update nav.php

--- nav.php
+++ nav.php
@@ -1,7 +1,8 @@
 <html>
     <body>
         <div id="nav">
-            
+            <div class="navElement">HOME</div>
+            <div class="navElement">BACK</div>
         </div>
     </body>
 </html>
